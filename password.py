4366740e2dc91e900bec03dd76ea77e20b563ea8
pingleee <v65224@gmail.com>
2022-08-14 22:29:14 +0900

    Revised

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3
 while i > 0:
+    i = i - 1
     enter = input('Please enter the password: ')
     if password == enter:
         print('Success')
         break
     else:
-        i = i - 1
-        print('Wrong! You left', i, 'times.')
+        print('Wrong!')
+        if i > 0:
+            print('You left', i, 'times.')
+        else:
+            print('Lock you account forever.')
